95ced429f369bf24d859c5306c3c10e7fd11547f
Ajaykumartatapudi <ajaykumar298901@gmail.com>
2025-11-02 23:41:42 +0530

    added the header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 
 import './App.css';
 import Home from './components/Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from './components/Header';
+
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <main className="flex-grow-1">
+
+
+        <Router>
+           <Header />
+          <Routes>
+           
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/news" element={<News />} />
+          <Route path="/sports" element={<Sports />} />
+          <Route path="/tech" element={<Tech />} />
+          <Route path="/jobs" element={<Jobs />} />
+          <Route path="/entertainment" element={<Entertainment />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/reporters" element={<Reporters />} /> */}
+          </Routes>
+        </Router>
+      </main>
     </div>
   );
 }
