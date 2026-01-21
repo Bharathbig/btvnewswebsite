9ec582b6693cb0615f775ce547f47965c9fd044b
Bharathbig <killibharath2001@gmail.com>
2026-01-21 18:12:31 +0530

    Update reporter contact details and names

--- src/components/Reporters.js
+++ src/components/Reporters.js
@@ -30,11 +30,11 @@ const Reporters = () => {
 
   const reporters = [
     { id: "25003", name: "K. Satyam", area: "Srikakulam District", contact: "+91 9440485370" },
-    { id: "25004", name: "B. Laxman Rao", area: "Visakhapatnam District", contact: "+91 8519822213" },
-    { id: "25005", name: "P. Vasu", area: "Ponduru Mandalam Srikakulam", contact: "+91 9676971477" },
+    { id: "25004", name: "B. Laxman Rao", area: "Visakhapatnam District", contact: "+91 9346139329" },
+    { id: "25005", name: "H. Shyamlarao", area: "Palakonda Mandalam Srikakulam", contact: "+91 9110352061" },
     { id: "25006", name: "P. Jagadeesh", area: "Gajuwaka Visakhapatnam", contact: "+91 8500231958" },
-    { id: "25007", name: "H. Lachumayya", area: "Vizianagaram District", contact: "+91 7382699452" },
-    { id: "25008", name: "P. Sunil", area: "Anakapalli District", contact: "+91 90032 10708" },
+    { id: "25007", name: "H. Lachumayya", area: "Vizianagaram District", contact: "+91 782699452" },
+    { id: "25008", name: "P. Sunil", area: "Anakapalli District", contact: "+91 9003210708" },
     { id: "25009", name: "H. Harsha", area: "Vijayawada District", contact: "+91 8523894251" },
     { id: "25010", name: "S. Bhaskhar", area: "Sarubujjili", contact: "+91 8523894251" },
   ];
